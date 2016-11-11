d55fddb69770391ebee46eb69f09496f83614b0c
李春鹏 <lcp0578@gmail.com>
2016-11-11 23:39:51 +0800

    add tick

---
+++ time/tick.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println(time.Now())
+	c := time.After(3 * time.Second) // 返回channel类型，3s后发送当前时间
+	t := <-c
+	fmt.Println(t)
+	nt := time.NewTimer(3 * time.Second) //返回Timer类型
+	ntt := nt.C
+	fmt.Println(ntt)
+	// time.AfterFunc(5*time.Second, Test)
+	// var str string
+	// fmt.Scan(&str)
+
+	tk := time.Tick(2 * time.Second)
+	for tkt := range tk {
+		fmt.Println(tkt)
+	}
+}
+
+func Test() {
+	fmt.Println("hello golang", time.Now())
+}
