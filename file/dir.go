65c90d99e8d7803d3a1b589ff24f487780285738
李春鹏 <lcp0578@gmail.com>
2016-11-17 17:49:55 +0800

    add read dir

---
+++ file/dir.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	f, openErr := os.OpenFile("D:\\gowork\\src", os.O_RDONLY, 0666)
+	if openErr != nil {
+		fmt.Println(openErr.Error())
+		return
+	}
+	arrFile, readErr := f.Readdir(0)
+	if readErr != nil {
+		fmt.Println(readErr.Error())
+		return
+	}
+	for k, v := range arrFile {
+		fmt.Println(k, "\t", v.Name(), "\t", v.IsDir())
+	}
+}
