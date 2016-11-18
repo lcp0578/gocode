cab89a6b833103906aa1a35632f56e8134fafcb0
李春鹏 <lcp0578@gmail.com>
2016-11-18 23:57:51 +0800

    modify func div

--- func/div.go
+++ func/div.go
@@ -12,12 +12,33 @@ func div(x, y int) (int, error) {
 	return x / y, nil
 }
 
+func div2(x, y int) (r int, err error) {
+	if y == 0 {
+		err = errors.New("division by zero")
+		return
+	}
+	r = x / y
+	return
+}
+func div3(x, y int) (r int, err error) {
+	if y == 0 {
+		err = errors.New("division by zero")
+		return
+	}
+	{
+		r := x / y // 新定义的同名局部变量，同名遮蔽
+		return r, err
+	}
+	return
+}
 func log(x int, err error) {
 	fmt.Println(x, err)
 }
 
 func test(x, y int) (int, error) {
-	return div(x, y) // 多返回值作为return结果
+	//return div(x, y) // 多返回值作为return结果
+	//return div2(x, y)
+	return div3(x, y)
 }
 
 func main() {
