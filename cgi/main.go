b7c35225300826a2a87f38bfb5cc1412ef55ae63
lcpeng <lcp0578@gmail.com>
2016-12-04 23:39:11 +0800

    modify cgi server

--- cgi/main.go
+++ cgi/main.go
@@ -10,8 +10,8 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		handler := new(cgi.Handler)
 		handler.Path = "/usr/local/go/bin/go"
-		script := "/home/root/cgi-script" + r.URL.Path
-		handler.Dir = "/home/root/cgi-script"
+		script := "/home/gowork/src/learn-go/cgi-script" + r.URL.Path
+		handler.Dir = "/home/gowork/src/learn-go/cgi-script"
 		args := []string{"run", script}
 		handler.Args = append(handler.Args, args...)
 		handler.ServeHTTP(w, r)
