cebe77a753430b9babadd843ce669035a7386c0e
lcp0578 <lcp0578@gmail.com>
2018-08-09 16:12:15 +0800

    [MOD] add hex

---
+++ hex/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"crypto/md5"
+)
+
+func main(){
+	//a := "30313233343536373238"
+	address := "0123456728"
+	by := []byte(address)
+	//fmt.Print(by)
+	h := md5.New()
+	str := hex.EncodeToString(h.Sum(by))
+	fmt.Print(str)
+	//bs, err := hex.DecodeString(a)
+	//src := make([]byte, 0)
+	//hex.Encode(by, src)
+	//fmt.Print(src)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//fmt.Println(string(bs))
+}
